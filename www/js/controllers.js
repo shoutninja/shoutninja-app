2f9663e47f89b62c3ac8be66d14b4935ba0709a9
Henry Kaufman <hkaufman19@gmail.com>
2014-12-19 22:37:32 -0700

    changed a shit load

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,47 @@
 angular.module('starter.controllers', ['ngRoute', 'firebase'])
 
-.controller('DashCtrl', function ($scope) {})
+.constant('fbURL', 'https://unknown-chat.firebaseio.com/')
 
-.controller('ChatsCtrl', function ($scope, $firebase) {
-    var chatURL = "https://unknown-chat.firebaseio.com/";
-    var ref = new Firebase(chatURL);
+.constant('chatURL', '/chats')
+
+.constant('eventURL', '/events')
+
+.controller('DashCtrl', function ($scope, $firebase, fbURL, eventURL) {
+    var ref = new Firebase(fbURL + eventURL);
+    var sync = $firebase(ref);
+
+    var events = sync.$asArray();
+
+    $scope.events = events;
+
+    //    $scope.getVotes = votes.getVotes;
+    //    $scope.getVotesArray = function (e) {
+    //        return $scope.getVotes(e).$asArray();
+    //    }
+
+    $scope.save = function (event) {
+        events.$add({
+            name: event.name,
+            description: event.description,
+            date: event.date
+        });
+
+    }
+    //    $scope.addVote = function (event) {
+    //        if (auth.authMessage()) {
+    //            var votesArray = $scope.getVotes(event);
+    //            var vote = new defaults.User();
+    //            vote.uid = auth.getAuth().uid;
+    //            vote.username = auth.getAuth().getUsername();
+    //            vote.provider = auth.getAuth().provider;
+    //            votesArray.$set(vote.uid, vote);
+    //
+    //        }
+    //    };
+})
+
+.controller('ChatsCtrl', function ($scope, $firebase, fbURL, chatURL) {
+    var ref = new Firebase(fbURL + chatURL);
     var sync = $firebase(ref);
 
     var chats = sync.$asArray();
@@ -32,8 +69,19 @@ angular.module('starter.controllers', ['ngRoute', 'firebase'])
     $scope.friend = Friends.get($stateParams.friendId);
 })
 
-.controller('AccountCtrl', function ($scope) {
-    $scope.settings = {
-        enableFriends: true
-    };
+.controller('AccountCtrl', function ($scope,$rootScope) {
+    $scope.login = function () {
+        var ref = new Firebase("https://unknown-chat.firebaseio.com");
+        ref.authWithOAuthPopup("twitter", function (error, authData) {
+            if (error) {
+                console.log("Login Failed!", error);
+            } else {
+                console.log("Authenticated successfully with payload:", authData);
+                alert(authData.twitter.username);
+                $rootScope.$apply(function() {
+                    $scope.authData = authData;
+                });
+            }
+        });
+    }
 });
